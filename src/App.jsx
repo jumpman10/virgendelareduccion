6bfd6462c89f4083a3bbe47628776d41c1ff81be
jumpman10 <jpmoyano10@gmail.com>
2023-07-31 00:48:42 -0300

    maqueta

--- src/App.jsx
+++ src/App.jsx
@@ -6,31 +6,24 @@ function App() {
 
   return (
     <div>
-      <div className='portada'>
-      <img src={virgen} alt="virgen" className='imgvirgen'></img>
-      <h1>Virgen del Valle de La Reducción</h1>
+      <div className='body-content'>
+        <div className='portada'>
+          <img src={virgen} alt="virgen" className='imgvirgen'></img>
+          <h1>Virgen del Valle de La Reducción</h1>
+        </div>
+        <div className='section'>
+          <div className='item-section-text'><a>La Historia del Hallazgo</a></div>
+          <div className='item-section-text'><a>Museo Virtual</a></div>
+          <div className='item-section-text'><a>La misión en el Santuario</a></div>
+          <div className='item-section-text'><a>Fiesta del 8 de Diciembre</a></div>
+        </div>
       </div>
-      <div className='section'>
-        <div className='item-section-text'><a>La Historia del Hallazgo</a></div>
-        <div className='item-section-text'><a>Museo Virtual</a></div>
-        <div className='item-section-text'><a>La misión en el Santuario</a></div>
-        <div className='item-section-text'><a>Fiesta del 8 de Diciembre</a></div>
+      <div className='body-content'>
       </div>
-      {/* <div className='cards-container'>
-        <div className='card'><a>La Historia del Hallazgo</a></div>
-        <div className='card'><a>La Historia del Hallazgo</a></div>
-        <div className='card'><a>La Historia del Hallazgo</a></div>
+      <div className='body-content'>
       </div>
-      <div className='cards-container'>
-        <div className='card'><a>La Historia del Hallazgo</a></div>
-        <div className='card'><a>La Historia del Hallazgo</a></div>
-        <div className='card'><a>La Historia del Hallazgo</a></div>
+      <div className='body-content'>
       </div>
-      <div className='cards-container'>
-        <div className='card'><a>La Historia del Hallazgo</a></div>
-        <div className='card'><a>La Historia del Hallazgo</a></div>
-        <div className='card'><a>La Historia del Hallazgo</a></div>
-      </div> */}
     </div>
   )
 }
